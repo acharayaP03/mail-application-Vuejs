b1b8e552c6ed232eaa77215d17e837de1d67c6ee
prabhakar acharya <prabhakaracharya@prabhakar-acharyas-MacBook-Pro.local>
2019-10-12 23:45:26 +1100

    added all messages to the view

--- src/Important.vue
+++ src/Important.vue
@@ -1,9 +1,28 @@
 <template>
-    <h1>Importance</h1>
+    <div class="inbox-body">
+        <app-messages :messages="importantMessages"></app-messages>
+    </div>
 </template>
 
 <script>
+    import Messages from "./Messages.vue";
+
     export default {
-        
+        props:{
+            data:{
+                type: Object,
+                required: true
+            }
+        },
+        computed:{
+            importantMessages(){
+              return this.data.messages.filter(function (message) {
+                    return (message.type == 'incoming' && message.isImportant && !message.isDelelted)                  
+              });
+          }
+        },
+        components: {
+            appMessages: Messages
+        }
     }
 </script>
